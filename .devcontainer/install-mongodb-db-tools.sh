597423c5c13cfa4a87ebb025e618870d14f3fafd
Diego Freniche <diego@freniche.com>
2025-03-06 11:56:42 -0500

    Fixed install MongoDB tools script

--- .devcontainer/install-mongodb-db-tools.sh
+++ .devcontainer/install-mongodb-db-tools.sh
@@ -10,9 +10,10 @@
 
 export MONGO_TOOLS_VERSION=100.11.0 
 export ARCH=$(dpkg --print-architecture) 
-if [ ARCH = "amd64" ]; then export TARGET_ARCH="x86_64"; else export TARGET_ARCH="arm64"; fi 
+if [ $ARCH = "amd64" ]; then export TARGET_ARCH="x86_64"; else export TARGET_ARCH="arm64"; fi 
 export TARGET="mongodb-database-tools-ubuntu2404-${TARGET_ARCH}-${MONGO_TOOLS_VERSION}.deb" 
-echo "Installing tools for architecture $ARCH, linux/${TARGET_ARCH}, target file=${TARGET}" 
+echo "Installing tools for architecture $ARCH, linux/${TARGET_ARCH}, target file=${TARGET}"
+echo "curl https://fastdl.mongodb.org/tools/db/${TARGET}" 
 curl "https://fastdl.mongodb.org/tools/db/${TARGET}" --output "${TARGET}" 
 sudo apt-get install -y "./${TARGET}" 
 rm "./${TARGET}"
