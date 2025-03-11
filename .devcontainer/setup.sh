4e8b95b89d4daa8540be701ba01164c5b24f421f
Diego Freniche <diego@freniche.com>
2025-03-11 10:23:23 +0000

    Added Deno Typescript kernel and 1st example

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 sudo apt-get update -y
+
+# Install Jupyter notebooks
 sudo apt-get install jupyter -y
+
+# Java Kernel
 sudo python3 ijava-1.3.0/install.py
-sudo apt install python3-pymongo -y
+
+# Python Kernel
+sudo apt-get install python3-pymongo -y
+
+# Install deno - Nodejs modern engine
+sudo apt-get install npm -y
+sudo npm install -g deno
+export PATH="$PATH:/home/vscode/lib/node_modules/deno"
+deno jupyter --install
+
+# Install MongoDB Deno driver
+sudo deno install --allow-net --allow-read https://deno.land/x/mongo@v0.31.1/mod.ts
